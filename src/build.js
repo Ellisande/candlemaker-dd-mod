b5337e4964932daf7599061a0be51a7030fbb5b3
Ellisande <justin.dragos@gmail.com>
2018-09-15 11:48:56 -0700

    Make the output even easier to use

--- src/build.js
+++ src/build.js
@@ -7,9 +7,15 @@ const enabled = ["'strike_match'", "'embers'"];
 
 const enabledSkills = skills.filter(skill => enabled.includes(skill.id));
 
-const skillsAsStrings = enabledSkills.map(convert.fromObject).join("\n");
+const skillsAsStrings = enabledSkills
+  .map(convert.fromObject)
+  .map(i => "combat_skill: " + i)
+  .join("\n");
 
-const effectsAsStrings = effects.map(convert.fromObject).join("\n");
+const effectsAsStrings = effects
+  .map(convert.fromObject)
+  .map(i => "effect: " + i)
+  .join("\n");
 
 fs.writeFileSync("./build/skills.info.darkest", skillsAsStrings);
 fs.writeFileSync("./build/effects.info.darkest", effectsAsStrings);
