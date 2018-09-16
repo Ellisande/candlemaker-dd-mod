6aba31b635f938f3af5d4a02d84b834a0ba7e1cd
Ellisande <justin.dragos@gmail.com>
2018-09-15 19:57:11 -0700

    Make the initial level 0 and the debuff amount static

--- src/effects/damageTaken.js
+++ src/effects/damageTaken.js
@@ -1,12 +1,12 @@
 const { scaleJustNames } = require("./effectUtils");
 
 const base = {
-  name: "'CM Damge Taken Debuff 1'",
+  name: "'CM Damge Taken Debuff 0'",
   target: "'target'",
   curio_result_type: "'negative'",
   chance: "100%",
   duration: "2",
-  buff_ids: "'DAMAGE_TAKEN_INCREASE_1'",
+  buff_ids: "'DAMAGE_TAKEN_INCREASE'",
   on_hit: "true",
   on_miss: "false",
   apply_once: "false"
