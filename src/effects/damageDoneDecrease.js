2e285486b5563f1bd5a10256663254e55069d737
Ellisande <justin.dragos@gmail.com>
2018-09-15 19:03:41 -0700

    Add a damage decrease effect

---
+++ src/effects/damageDoneDecrease.js
@@ -0,0 +1,19 @@
+const { scaleJustNames } = require("./effectUtils");
+
+const base = {
+  name: "'CM Damage Done Decrease 0'",
+  target: "'target'",
+  curio_result_type: "'negative'",
+  chance: "100%",
+  duration: "1",
+  buff_ids: "'DAMAGE_DONE_DECREASE_0'",
+  on_hit: "true",
+  on_miss: "true",
+  apply_once: "false"
+};
+
+const allLevels = scaleJustNames(base);
+
+// console.log(allLevels);
+
+module.exports = allLevels;
