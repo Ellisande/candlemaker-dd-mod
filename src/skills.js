3e274c460cc527f7fc1ef40de5fd8dcd1ab26ae0
Ellisande <justin.dragos@gmail.com>
2018-09-15 17:28:45 -0700

    Changed skills to be a js file instead of json

---
+++ src/skills.js
@@ -0,0 +1,6 @@
+const embers = require("./skills/embers");
+const snuff = require("./skills/snuff");
+const strikeMatch = require("./skills/strikeMatch");
+const blazingStar = require("./skills/blazingStar");
+
+module.exports = [...embers, ...snuff, ...strikeMatch, ...blazingStar];
