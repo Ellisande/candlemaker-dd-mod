93994e5fbe199147332ad2a0399eacadaf0d2331
Ellisande <justin.dragos@gmail.com>
2018-09-15 17:30:44 -0700

    Initial shot at snuff

---
+++ src/skills/snuff.js
@@ -0,0 +1,29 @@
+const { effectsScale, generateLevels } = require("./skillUtils");
+
+const base = {
+  id: "'snuff'",
+  level: "0",
+  self_target_valid: "false",
+  launch: "34",
+  target: "@123",
+  is_crit_valid: "False",
+  effect: [
+    "'Clearguard'",
+    "'CM Snuff Torch 0",
+    "'Stealth'",
+    "'CM Damage Done Decrease 0",
+    "'CM Poised 0'",
+    "'CM Poised 0'"
+  ],
+  is_stall_invalidating: "false"
+};
+
+const allLevels = generateLevels(level => ({
+  ...base,
+  level,
+  effect: effectsScale(base.effect, level)
+}));
+
+console.log(allLevels);
+
+module.exports = allLevels;
