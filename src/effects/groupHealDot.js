d7c0e60eaae92f476ca90f28d95b97912a01b6b2
Ellisande <justin.dragos@gmail.com>
2018-09-15 19:11:41 -0700

    Add the group heal for blazing star

---
+++ src/effects/groupHealDot.js
@@ -0,0 +1,35 @@
+const { generateLevels, nameScale } = require("./effectUtils");
+
+const healBase = {
+  low: 1,
+  high: 3
+};
+const base = {
+  name: "'CM Group Healdot 0'",
+  target: "'performer_group'",
+  curio_result_type: "'positive'",
+  chance: "100%",
+  dotHpHeal: `${healBase.low} ${healBase.high}`,
+  duration: "2",
+  on_hit: "true",
+  on_miss: "true"
+};
+
+const healScale = level => {
+  const offset = Math.floor(level / 2);
+  const scaled = {
+    low: healBase.low + offset,
+    high: healBase.high + offset
+  };
+  return `${scaled.low} ${scaled.high}`;
+};
+
+const allLevels = generateLevels(level => ({
+  ...base,
+  name: nameScale(base.name, level),
+  dotHpHeal: healScale(level)
+}));
+
+// console.log(allLevels);
+
+module.exports = allLevels;
