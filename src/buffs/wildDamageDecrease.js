771284f9532c7835a4deb39fd57eaae5f2ae83a9
Ellisande <justin.dragos@gmail.com>
2018-09-15 22:16:01 -0700

    Tune down the wild damage decrease

--- src/buffs/wildDamageDecrease.js
+++ src/buffs/wildDamageDecrease.js
@@ -3,7 +3,7 @@ const base = {
   id: "WILD_MIN_DAMAGE_DONE_DEBUFF_0",
   stat_type: "combat_stat_multiply",
   stat_sub_type: "damage_low",
-  amount: -0.25,
+  amount: -0.2,
   duration: 3,
   remove_if_not_active: false,
   rule_type: "lightbelow",
@@ -16,7 +16,7 @@ const base = {
 
 const amountScale = (base, level) => {
   const offset = level * 0.05;
-  return base - offset;
+  return base + offset;
 };
 
 const allLevels = generateLevels(level => ({
