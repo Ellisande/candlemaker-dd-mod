7327672e53ae06291e745ecd138a5a42c7db9f8f
Ellisande <justin.dragos@gmail.com>
2018-09-15 20:05:16 -0700

    Build the buffs.json

--- src/build.js
+++ src/build.js
@@ -2,6 +2,7 @@ const convert = require("./convert");
 const fs = require("fs");
 const skills = require("./skills");
 const effects = require("./effects");
+const buffs = require("./buffs");
 
 const skillsAsStrings = skills
   .map(convert.fromObject)
@@ -15,3 +16,4 @@ const effectsAsStrings = effects
 
 fs.writeFileSync("./build/skills.info.darkest", skillsAsStrings);
 fs.writeFileSync("./build/effects.info.darkest", effectsAsStrings);
+fs.writeFileSync("./build/buffs.json", JSON.stringify(buffs, null, 2));
