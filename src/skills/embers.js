3a6bc26d9a139b17d1ca0c97f686e3e4699d9a87
Ellisande <justin.dragos@gmail.com>
2018-09-15 17:30:19 -0700

    Create scaling embers in its own file

---
+++ src/skills/embers.js
@@ -0,0 +1,37 @@
+const {
+  atkScale,
+  effectsScale,
+  critScale,
+  generateLevels
+} = require("./skillUtils");
+
+const base = {
+  id: "'embers'",
+  level: "0",
+  type: "'ranged'",
+  atk: "95%",
+  dmg: "-40%",
+  crit: "5%",
+  launch: "23",
+  target: "~23",
+  is_crit_valid: "True",
+  effect: [
+    "'CM Torch Decrease Minor 1'",
+    "'CM Damge Taken Debuff 1'",
+    "'CM Poised 1'"
+  ],
+  generation_guaranteed: "true"
+};
+
+const allLevels = generateLevels(level => ({
+  ...base,
+  level,
+  atk: atkScale(base.atk, level),
+  crit: critScale(base.crit, level),
+  effect: effectsScale(base.effect, level),
+  generation_guaranteed: level === 0 ? "true" : "false"
+}));
+
+console.log(allLevels);
+
+module.exports = allLevels;
