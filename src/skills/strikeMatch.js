1466af52a2a3e877a8fe17de55e209bed53982f0
Ellisande <justin.dragos@gmail.com>
2018-09-15 17:30:34 -0700

    Create scaling strike match in its own file

---
+++ src/skills/strikeMatch.js
@@ -0,0 +1,30 @@
+const { effectsScale, generateLevels } = require("./skillUtils");
+
+const base = {
+  id: "'strike_match'",
+  level: "0",
+  type: "'melee'",
+  heal: "0 1",
+  self_target_valid: "true",
+  launch: "1234",
+  target: "@1234",
+  is_crit_valid: "True",
+  effect: ["'CM Torch Increase Minor 1'", "'CM Healdot 1'", "'CM Wild 1'"],
+  is_stall_invalidating: "false"
+};
+
+const scaleHeal = level => {
+  const increase = Math.floor(level / 2);
+  return `${0 + increase} ${1 + increase}`;
+};
+
+const allLevels = generateLevels(level => ({
+  ...base,
+  level,
+  heal: scaleHeal(level),
+  effect: effectsScale(base.effect, level)
+}));
+
+console.log(allLevels);
+
+module.exports = allLevels;
