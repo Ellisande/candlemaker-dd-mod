97b6a3371158bf1e1b94524f8c8821a93a3f498c
Ellisande <justin.dragos@gmail.com>
2018-09-15 18:49:41 -0700

    Add a targetted stealth effect

---
+++ src/effects/stealth.js
@@ -0,0 +1,11 @@
+const base = {
+  name: "'Stealth'",
+  target: "'target'",
+  chance: "100%",
+  stealth: "1",
+  on_hit: "true",
+  on_miss: "false",
+  duration: "2"
+};
+
+module.exports = [base];
