af00d4ff7f6e7b484f0fc6ce1ccf8f126f2782ac
Ellisande <justin.dragos@gmail.com>
2018-09-15 18:32:43 -0700

    Add a helper method when just names need to scale

--- src/effects/effectUtils.js
+++ src/effects/effectUtils.js
@@ -3,7 +3,17 @@ const generateLevels = levelMap => _.range(0, 5).map(levelMap);
 
 const nameScale = (effectName, level) => effectName.replace(/\d/, level);
 
+const scaleJustNames = baseEffect =>
+  generateLevels(level => ({
+    ...baseEffect,
+    name: nameScale(baseEffect.name, level),
+    buff_ids: Array.isArray(baseEffect.buff_ids)
+      ? baseEffect.buff_ids.map(effect => nameScale(effect, level))
+      : nameScale(baseEffect.buff_ids, level)
+  }));
+
 module.exports = {
   generateLevels,
-  nameScale
+  nameScale,
+  scaleJustNames
 };
