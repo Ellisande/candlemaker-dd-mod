63e74533820a48dbe7eca9ba8ed03acd08038fd4
Ellisande <justin.dragos@gmail.com>
2018-09-18 09:05:59 -0700

    Modify the wild buffs to only last 2 turns

--- src/effects/wild.js
+++ src/effects/wild.js
@@ -5,7 +5,7 @@ const base = {
   target: "'performer_group'",
   curio_result_type: "'positive'",
   chance: "100%",
-  duration: "3",
+  duration: "2",
   buff_ids: ["'WILD_DAMAGE_DONE_BUFF_0'", "'WILD_MIN_DAMAGE_DONE_DEBUFF_0'"],
   on_hit: "true",
   on_miss: "true",
