3a7ed8dc10ef913fee61ca6b6948543d00d9528d
Ellisande <justin.dragos@gmail.com>
2018-09-18 14:46:57 -0700

    Buffs are json didn't need to double escape

--- src/buffs/disableHealing.js
+++ src/buffs/disableHealing.js
@@ -1,12 +1,12 @@
 const { generateLevels, nameScale } = require("../effects/effectUtils");
 
 const base = {
-  id: "'DISABLE_HEALING_DEBUFF'",
+  id: "DISABLE_HEALING_DEBUFF",
   duration: 1,
   remove_if_not_active: true,
   rule_type: "lightabove",
   is_false_rule: false,
-  disable_combat_skill_attribute: "'heal'",
+  disable_combat_skill_attribute: "heal",
   rule_data: {
     float: 50,
     string: ""
