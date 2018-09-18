57c9b9c11ad9eaca9e2328d37329cb47e4dac735
Ellisande <justin.dragos@gmail.com>
2018-09-18 08:25:12 -0700

    Add a buff type that clears marks

---
+++ src/effects/clearmark.js
@@ -0,0 +1,13 @@
+const base = {
+  name: "'Clearmark'",
+  target: "'global'",
+  chance: "100%",
+  curio_result_type: "'neutral'",
+  on_hit: "true",
+  on_miss: "true",
+  apply_once: "true",
+  queue: "true",
+  untag: 1
+};
+
+module.exports = [base];
