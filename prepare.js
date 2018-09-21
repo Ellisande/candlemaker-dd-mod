68ba2d4458bcadf710c865ce868b115533da8530
Ellisande <justin.dragos@gmail.com>
2018-09-21 15:01:59 -0700

    Roll back the localization because it doesn't seem to work

--- prepare.js
+++ prepare.js
@@ -10,9 +10,10 @@ fs.copyFileSync(
   "./candlemaker/shared/buffs/base.buffs.json"
 );
 
-fs.copyFileSync(
-  "./build/candlemaker.string_table.xml",
-  "./candlemaker/localization/candlemaker.string_table.xml"
-);
+// Remove localization for now since it doesn't seem to work correctly
+// fs.copyFileSync(
+//   "./build/candlemaker.string_table.xml",
+//   "./candlemaker/localization/candlemaker.string_table.xml"
+// );
 
 // Need to copy over the class, but we don't have the build for it yet.
