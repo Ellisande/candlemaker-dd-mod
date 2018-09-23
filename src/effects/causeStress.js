f53895747798cb1b98ef29f3e7fbac291d5a7d31
Ellisande <justin.dragos@gmail.com>
2018-09-22 22:23:26 -0700

    Add a slight stress for boogeymen

---
+++ src/effects/causeStress.js
@@ -0,0 +1,11 @@
+const base = {
+  name: "'CM Boogeymen Target Stress'",
+  target: "'target'",
+  curio_result_type: "'negative'",
+  chance: "100%",
+  stress: 7,
+  on_hit: "true",
+  on_miss: "true"
+};
+
+module.exports = [base];
