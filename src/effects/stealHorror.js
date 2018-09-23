06dcbc56930089a74544af46dc17056efa2613d5
Ellisande <justin.dragos@gmail.com>
2018-09-23 10:18:26 -0700

    Add an effect for transferring horror for new mere trickery

---
+++ src/effects/stealHorror.js
@@ -0,0 +1,12 @@
+const base = {
+  name: "'CM Steal Horror'",
+  target: "'target_group_other'",
+  curio_result_type: "'negative'",
+  chance: "100%",
+  on_hit: "true",
+  on_miss: "true",
+  apply_once: "true",
+  steal_buff_stat_type: "'stress_dot'"
+};
+
+module.exports = [base];
