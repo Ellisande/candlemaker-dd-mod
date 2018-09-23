4fa39193e343a64091ed61eedab4cc52255b10d3
Ellisande <justin.dragos@gmail.com>
2018-09-22 22:25:17 -0700

    Rename since it isn't really wild

--- src/effects/boogeymen.js
+++ src/effects/boogeymen.js
@@ -1,11 +1,12 @@
 const { scaleJustNames } = require("./effectUtils");
 
 const base = {
-  name: "'CM Wild Single Target 0'",
+  name: "'CM Boogeymen 0'",
   target: "'target'",
   curio_result_type: "'positive'",
   chance: "100%",
   duration: "1",
+  push: 1,
   buff_ids: [
     "'BOOGEYMEN_DAMAGE_DONE_BUFF_0'",
     "'BOOGEYMEN_MIN_DAMAGE_DONE_DEBUFF_0'"
