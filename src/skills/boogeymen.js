7a0e9c851b28c767c96ac1656d43902d0868df55
Ellisande <justin.dragos@gmail.com>
2018-09-22 22:02:44 -0700

    Rename to boogeymen

---
+++ src/skills/boogeymen.js
@@ -0,0 +1,20 @@
+// Not done yet
+const base = {
+  id: "'boogeymen'",
+  level: "0",
+  atk: "0%",
+  dmg: "0%",
+  crit: "0%",
+  self_target_valid: "true",
+  launch: "123",
+  target: "@1234",
+  is_crit_valid: "True",
+  effect: [
+    "'CM Torch Decrease Minor 0",
+    "'CM Boogeymen Target Stress'",
+    "'CM Single Min Damage Done Debuff 0'",
+    "'CM Single Max Damage Done Buff 0'",
+    "'CM Poised 0'"
+  ],
+  is_stall_invalidating: "false"
+};
