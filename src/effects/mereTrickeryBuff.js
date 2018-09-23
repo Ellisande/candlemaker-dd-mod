d0a801953eb0021a5f5c012520e5a150dee7f750
Ellisande <justin.dragos@gmail.com>
2018-09-23 10:21:21 -0700

    Rework mere trickery to buff accuracy, virtue chance, and reduce horror stress

---
+++ src/effects/mereTrickeryBuff.js
@@ -0,0 +1,17 @@
+const base = {
+  name: "'CM Mere Trickery Buff 0'",
+  target: "'target'",
+  curio_result_type: "'positive'",
+  chance: "100%",
+  duration: "3",
+  on_hit: "true",
+  on_miss: "true",
+  apply_once: "true",
+  buff_ids: [
+    "'CM_HORROR_REDUCTION_BUFF_0'",
+    "'CM_ACC_BUFF_0'",
+    "'CM_VIRTUE_BUFF_0'"
+  ]
+};
+
+module.exports = [base];
