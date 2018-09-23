3bd0e2929f20706e062f3e565b5ff65adf76e10f
Ellisande <justin.dragos@gmail.com>
2018-09-22 23:08:47 -0700

    Have to pull if we want it to move forward

--- src/effects/boogeymen.js
+++ src/effects/boogeymen.js
@@ -6,7 +6,7 @@ const base = {
   curio_result_type: "'positive'",
   chance: "100%",
   duration: "1",
-  push: 1,
+  pull: 1,
   buff_ids: [
     "'BOOGEYMEN_DAMAGE_DONE_BUFF_0'",
     "'BOOGEYMEN_MIN_DAMAGE_DONE_DEBUFF_0'"
