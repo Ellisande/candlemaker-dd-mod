9f27fab52b0489684e196acab16f771158344210
Ellisande <justin.dragos@gmail.com>
2018-09-22 22:01:40 -0700

    Check stress disable to actually stop the use of skills

--- src/buffs/stressDisableDebuff.js
+++ src/buffs/stressDisableDebuff.js
@@ -1,7 +1,7 @@
 const base = {
   id: "STRESS_DISABLE_DEBUFF",
-  stat_type: "stress_dmg_percent",
-  stat_sub_type: "",
+  stat_type: "disable_combat_skill_attribute",
+  stat_sub_type: "stress",
   amount: -1.0,
   duration: 2,
   remove_if_not_active: false,
