20f94ded2567660e7e9516c4c7b655938446e869
Ellisande <justin.dragos@gmail.com>
2018-09-24 22:10:53 -0700

    Rework embers to see if I can get it to do what I want

--- src/skills/embers.js
+++ src/skills/embers.js
@@ -9,14 +9,12 @@ const base = {
   id: "'embers'",
   level: "0",
   type: "'ranged'",
-  atk: "90%",
-  dmg: "-50%",
+  atk: "60%",
+  dmg: "-60%",
   crit: "3%",
   launch: "23",
-  target: "1234",
+  target: "@1234",
   is_crit_valid: "True",
-  extra_targets_count: 2,
-  extra_targets_chance: 1,
   effect: [
     "'CM Torch Decrease Minor 0'",
     "'CM Damge Taken Debuff 0'",
@@ -31,10 +29,9 @@ const allLevels = generateLevels(level => ({
   atk: atkScale(base.atk, level),
   crit: critScale(base.crit, level),
   effect: effectsScale(base.effect, level),
-  generation_guaranteed: level === 0 ? "true" : "false",
-  extra_targets_chance: base.extra_targets_chance + level * 0.05
+  generation_guaranteed: level === 0 ? "true" : "false"
 }));
 
-// console.log(allLevels);
+console.log(allLevels);
 
 module.exports = allLevels;
