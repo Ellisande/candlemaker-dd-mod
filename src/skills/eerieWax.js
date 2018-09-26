c5386f9c4641f68ff244d5f4826edf3133e85edc
Ellisande <justin.dragos@gmail.com>
2018-09-25 20:43:14 -0700

    New skill: eerie wax

---
+++ src/skills/eerieWax.js
@@ -0,0 +1,38 @@
+const {
+  atkScale,
+  effectsScale,
+  critScale,
+  generateLevels
+} = require("./skillUtils");
+
+const base = {
+  id: "'eerie_wax'",
+  level: "0",
+  type: "'melee'",
+  atk: "95%",
+  dmg: "0%",
+  crit: "3%",
+  launch: "1234",
+  rank_damage_modifiers: "-50% -25% 25% 50%",
+  target: "1234",
+  is_crit_valid: "True",
+  effect: [
+    "'CM Torch Decrease Minor 0'",
+    "'CM Steal Effects 0'",
+    "'CM Poised 0'"
+  ],
+  generation_guaranteed: "true"
+};
+
+const allLevels = generateLevels(level => ({
+  ...base,
+  level,
+  atk: atkScale(base.atk, level),
+  crit: critScale(base.crit, level),
+  effect: effectsScale(base.effect, level),
+  generation_guaranteed: level === 0 ? "true" : "false"
+}));
+
+// console.log(allLevels);
+
+module.exports = allLevels;
