78867b37f2280ad49e0481dec23a04cc3a9d1ac5
Ellisande <justin.dragos@gmail.com>
2018-09-27 20:39:11 -0700

    Trying one more time on steal blight and bleed

--- src/effects/stealBleed.js
+++ src/effects/stealBleed.js
@@ -2,7 +2,7 @@ const { generateLevels, nameScale } = require("./effectUtils");
 
 const base = {
   name: "'CM Steal Bleed 0'",
-  target: "'target_enemy_group'",
+  target: "'target_group'",
   curio_result_type: "'positive'",
   chance: "100%",
   on_hit: "true",
