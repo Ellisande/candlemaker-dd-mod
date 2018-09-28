a5750356b049211f54688333b1f4814a01bc503b
Ellisande <justin.dragos@gmail.com>
2018-09-27 18:56:12 -0700

    Try target enemy group

--- src/effects/stealBleed.js
+++ src/effects/stealBleed.js
@@ -2,7 +2,7 @@ const { generateLevels, nameScale } = require("./effectUtils");
 
 const base = {
   name: "'CM Steal Bleed 0'",
-  target: "'performer_group'",
+  target: "'target_enemy_group'",
   curio_result_type: "'positive'",
   chance: "100%",
   on_hit: "true",
