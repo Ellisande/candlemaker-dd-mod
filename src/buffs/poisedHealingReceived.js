44ff4c09101ba8b2eab8c9cf5417d90302a67592
Ellisande <justin.dragos@gmail.com>
2018-09-29 11:58:10 -0700

    Remove the poised scaling. Add a double variation.

--- src/buffs/poisedHealingReceived.js
+++ src/buffs/poisedHealingReceived.js
@@ -1,7 +1,7 @@
 const { generateLevels, nameScale } = require("../effects/effectUtils");
 
-const base = {
-  id: "POISED_HEALING_TAKEN_BUFF_0",
+const single = {
+  id: "POISED_HEALING_TAKEN_BUFF_SINGLE",
   stat_type: "hp_heal_received_percent",
   stat_sub_type: "",
   amount: 0.25,
@@ -15,18 +15,21 @@ const base = {
   }
 };
 
-// Might want to try scaling again later
-const amountScale = (base, level) => {
-  return base;
-  // const offset = level * 0.05;
-  // return base + offset;
+const double = {
+  id: "POISED_HEALING_TAKEN_BUFF_DOUBLE",
+  stat_type: "hp_heal_received_percent",
+  stat_sub_type: "",
+  amount: 0.5,
+  duration: 3,
+  remove_if_not_active: false,
+  rule_type: "lightabove",
+  is_false_rule: false,
+  rule_data: {
+    float: 49,
+    string: ""
+  }
 };
 
-const allLevels = generateLevels(level => ({
-  ...base,
-  id: nameScale(base.id, level)
-}));
+// console.log(single, double);
 
-// console.log(allLevels);
-
-module.exports = allLevels;
+module.exports = [single, double];
