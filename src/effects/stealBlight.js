42443ad2445146c056bc89176c06d5dc5c5009f1
Ellisande <justin.dragos@gmail.com>
2018-09-28 20:40:41 -0700

    Change steal bleed / blight to pull from the enemy onto the candlemaker

--- src/effects/stealBlight.js
+++ src/effects/stealBlight.js
@@ -2,14 +2,13 @@ const { generateLevels, nameScale } = require("./effectUtils");
 
 const base = {
   name: "'CM Steal Blight 0'",
-  target: "'target_group'",
-  curio_result_type: "'positive'",
+  target: "'target'",
+  curio_result_type: "'negative'",
   chance: "100%",
   on_hit: "true",
   on_miss: "false",
   apply_once: "true",
-  steal_buff_stat_type: "'hp_dot_poison'",
-  swap_source_and_target: true
+  steal_buff_stat_type: "'hp_dot_poison'"
 };
 
 const allLevels = generateLevels(level => ({
