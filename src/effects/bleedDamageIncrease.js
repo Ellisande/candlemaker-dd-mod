a45bc1a79647173663b018d635f4422835285681
Ellisande <justin.dragos@gmail.com>
2018-09-29 22:44:44 -0700

    Bug, chance should be static 100%

--- src/effects/bleedDamageIncrease.js
+++ src/effects/bleedDamageIncrease.js
@@ -1,13 +1,12 @@
 const { generateLevels, nameScale } = require("./effectUtils");
 
 const baseDamageBuff = 230;
-const damageScalar = 20;
 
 const base = {
   name: "'CM Bleed Damage Increase 0'",
   target: "'performer'",
   curio_result_type: "'positive'",
-  chance: `${baseDamageBuff}%`,
+  chance: "100%",
   keyStatus: "'bleeding'",
   combat_stat_buff: 1,
   damage_low_multiply: `${baseDamageBuff}%`,
