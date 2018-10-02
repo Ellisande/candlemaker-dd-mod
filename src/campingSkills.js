ca425fa00cfa2599d9d15d1da2cf0179bc0f01c1
Ellisande <justin.dragos@gmail.com>
2018-10-01 21:32:10 -0700

    Add sixteen candles to the export

--- src/campingSkills.js
+++ src/campingSkills.js
@@ -1,11 +1,14 @@
 const sharedSkills = require("./camping/shared");
 const waxCoating = require("./camping/waxCoating");
+const sixteenCandles = require("./camping/sixteenCandles");
 
 const allSkills = {
   configuration: {
     class_specific_number_of_classes_threshold: 4
   },
-  skills: [...sharedSkills, ...waxCoating]
+  skills: [...sharedSkills, ...waxCoating, ...sixteenCandles]
 };
+
+// console.log(allSkills);
 
 module.exports = allSkills;
