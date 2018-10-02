02886920da8c75eed97c1224b73aa18f7ae05e48
Ellisande <justin.dragos@gmail.com>
2018-10-01 20:16:37 -0700

    Reduce the total healing from blazing star. Make it burn torch faster to reduce its use as an ending move

--- src/skills/blazingStar.js
+++ src/skills/blazingStar.js
@@ -1,13 +1,24 @@
 const { critScale, effectsScale, generateLevels } = require("./skillUtils");
 
-const baseHeal = {
-  low: 1,
-  high: 3
+const lowHeal = {
+  0: 0,
+  1: 1,
+  2: 1,
+  3: 2,
+  4: 2
+};
+
+const highHeal = {
+  0: 2,
+  1: 3,
+  2: 3,
+  3: 4,
+  4: 4
 };
 const base = {
   id: "'blazing_star'",
   level: "0",
-  heal: `${baseHeal.low} ${baseHeal.high}`,
+  heal: `${lowHeal[0]} ${highHeal[0]}`,
   crit: "5%",
   self_target_valid: "true",
   launch: "432",
@@ -17,24 +28,16 @@ const base = {
     "'CM Blazing Torch 0'",
     "'CM Group Healdot 0'",
     "'CM Wild Double'",
-    "'Disable Healing Abilities'"
+    "'Disable Healing Abilities'",
+    "'Increase Torch Burn'"
   ],
   is_stall_invalidating: "true"
-};
-
-const scaleHeal = level => {
-  const offset = level;
-  const scaled = {
-    low: baseHeal.low + offset,
-    high: baseHeal.high + offset
-  };
-  return `${scaled.low} ${scaled.high}`;
 };
 
 const allLevels = generateLevels(level => ({
   ...base,
   level,
-  heal: scaleHeal(level),
+  heal: `${lowHeal[level]} ${highHeal[level]}`,
   crit: critScale(base.crit, level),
   effect: effectsScale(base.effect, level)
 }));
