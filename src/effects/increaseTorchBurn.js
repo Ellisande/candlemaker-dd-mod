861a91a5e17cb3daf0c65f9b99f601952e80e19c
Ellisande <justin.dragos@gmail.com>
2018-10-01 20:15:49 -0700

    Add a debuff that increases torch burn by 150% for 2 rounds

---
+++ src/effects/increaseTorchBurn.js
@@ -0,0 +1,15 @@
+const base = {
+  name: "'Increase Torch Burn'",
+  target: "'performer'",
+  curio_result_type: "'negative'",
+  chance: "100%",
+  duration: "2",
+  buff_type: "torchlight_burn_percent",
+  buff_amount: "150%",
+  on_hit: "true",
+  on_miss: "false"
+};
+
+// console.log([base]);
+
+module.exports = [base];
