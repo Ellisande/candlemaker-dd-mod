c9d0e9cc43fa1f1a9ae4cf8a1ad835bacdf9da4a
Ellisande <justin.dragos@gmail.com>
2018-10-02 22:16:00 -0700

    Enable sixteen candles and flickering light for export

--- src/campingSkills.js
+++ src/campingSkills.js
@@ -1,15 +1,16 @@
 const sharedSkills = require("./camping/shared");
 const waxCoating = require("./camping/waxCoating");
 const sixteenCandles = require("./camping/sixteenCandles");
-
+const flickeringLight = require("./camping/flickeringLight");
 const allSkills = {
   configuration: {
     class_specific_number_of_classes_threshold: 4
   },
   skills: [
     ...sharedSkills,
-    ...waxCoating
-    // ...sixteenCandles
+    ...waxCoating,
+    ...sixteenCandles,
+    ...flickeringLight
   ]
 };
 
