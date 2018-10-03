7f75fb1013b217f0d00457e3979142226da63c76
Ellisande <justin.dragos@gmail.com>
2018-10-02 22:08:27 -0700

    Add a new camping skill that buffs ones unit with wild and poised

---
+++ src/camping/flickeringLight.js
@@ -0,0 +1,56 @@
+const flickering = {
+  id: "flickering_light",
+  level: 0,
+  cost: 4,
+  use_limit: 1,
+  effects: [
+    {
+      selection: "party_other",
+      requirements: [],
+      chance: {
+        code: "a",
+        amount: 1
+      },
+      type: "buff",
+      sub_type: "CAMPING_POISED_HEALING_TAKEN_BUFF_SINGLE",
+      amount: 1
+    },
+    {
+      selection: "party_other",
+      requirements: [],
+      chance: {
+        code: "b",
+        amount: 1
+      },
+      type: "buff",
+      sub_type: "CAMPING_WILD_MAX_DAMAGE_DONE_BUFF_SINGLE",
+      amount: 1
+    },
+    {
+      selection: "party_other",
+      requirements: [],
+      chance: {
+        code: "c",
+        amount: 1
+      },
+      type: "buff",
+      sub_type: "CAMPING_WILD_MIN_DAMAGE_DONE_BUFF_SINGLE",
+      amount: 1
+    }
+  ],
+  hero_classes: ["candlemaker"],
+  upgrade_requirements: [
+    {
+      code: "0",
+      currency_cost: [
+        {
+          type: "gold",
+          amount: 1750
+        }
+      ],
+      prerequisite_requirements: []
+    }
+  ]
+};
+
+module.exports = [flickering];
