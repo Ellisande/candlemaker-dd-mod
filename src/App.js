09ac63f0ef4f23b94761e3bce626cb561edd9ddb
Ellisande <justin.dragos@gmail.com>
2018-10-06 09:58:40 -0700

    Remove the active effect tracking

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { withState, compose, withProps } from "recompose";
 import { connect, Provider } from "react-redux";
-import logo from "./logo.svg";
 import "./App.css";
 import JsonPretty from "react-json-pretty";
 import buffs from "./buffs";
@@ -11,9 +10,6 @@ import hydrateSkill from "./stitch";
 import store from "./stores";
 
 const hydratedSkills = skills.map(hydrateSkill);
-
-const darkness = effects.find(i => i.name === "'Darkness 3'");
-const light = effects.find(i => i.name === "'Light 3'");
 
 const RawCollapse = ({ collapse, toggleCollapse, title, children }) => {
   return (
@@ -56,87 +52,6 @@ const RawAction = ({ use, skill, effect, className }) => {
   );
 };
 
-const activeTorch = (buff, torch) =>
-  (buff.rule_type === "lightabove" && torch > buff.rule_data.float) ||
-  (buff.rule_type === "lightbelow" && torch < buff.rule_data.float);
-const activeBuffSelector = state => {
-  const initialBuffs = state.buffs;
-  const onlyActive = initialBuffs.filter(buff =>
-    activeTorch(buff, state.torch)
-  );
-  return {
-    items: onlyActive
-  };
-};
-const ActiveBuffs = connect(activeBuffSelector)(List);
-
-const activeStatsSelector = state => {
-  const initialBuffs = state.buffs;
-  const onlyActive = initialBuffs.filter(buff =>
-    activeTorch(buff, state.torch)
-  );
-  const stats = onlyActive.reduce((stats, buff) => {
-    const type =
-      buff.stat_type + (buff.stat_sub_type ? ": " + buff.stat_sub_type : "");
-    return {
-      ...stats,
-      [type]: (stats[type] || 0) + buff.amount
-    };
-  }, {});
-  return {
-    stats
-  };
-};
-
-const RawStats = ({ stats }) =>
-  Object.keys(stats).map(key => (
-    <div className="light">
-      {key}: {stats[key].toFixed(2)}
-    </div>
-  ));
-
-const ActiveStats = connect(activeStatsSelector)(RawStats);
-
-const ReduceTorch = compose(
-  connect(
-    () => ({}),
-    (dispatch, props) => ({
-      use: () =>
-        dispatch({
-          type: "EFFECT",
-          buffs: [],
-          effects: [darkness]
-        })
-    })
-  ),
-  withProps({
-    skill: {
-      id: "Reduce Torch"
-    },
-    className: "torch-modifier"
-  })
-)(RawAction);
-
-const IncreaseTorch = compose(
-  connect(
-    () => ({}),
-    (dispatch, props) => ({
-      use: () =>
-        dispatch({
-          type: "EFFECT",
-          buffs: [],
-          effects: [light]
-        })
-    })
-  ),
-  withProps({
-    skill: {
-      id: "Increase Torch"
-    },
-    className: "torch-modifier"
-  })
-)(RawAction);
-
 const Action = connect(
   () => ({}),
   (dispatch, { skill }) => {
@@ -158,38 +73,12 @@ const Action = connect(
   }
 )(RawAction);
 
-const RawTorch = ({ torch }) => (
-  <div>
-    <div>Torch light: {torch}</div>
-  </div>
-);
-
-const Torch = connect(state => ({
-  torch: state.torch
-}))(RawTorch);
-
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div>
           <h3>Candle Maker</h3>
-          <div>
-            <IncreaseTorch />
-            <ReduceTorch />
-            <Torch />
-          </div>
-
-          <div className="current-state">
-            <div className="column">
-              <div>Active Buffs</div>
-              <ActiveBuffs />
-            </div>
-            <div className="column">
-              <div>Active Stats</div>
-              <ActiveStats />
-            </div>
-          </div>
           <div>
             {hydratedSkills.map(skill => (
               <Action key={skill.id} skill={skill} />
