fe62fce5b04ddb36d012cc91255fbe089077d27d
Ellisande <justin.dragos@gmail.com>
2018-10-16 21:58:03 -0700

    Remove stress reduction from mere trickery

--- src/effects/mereTrickeryBuff.js
+++ src/effects/mereTrickeryBuff.js
@@ -9,7 +9,6 @@ const base = {
   apply_once: "true",
   clearDotStress: 1,
   buff_ids: [
-    "'CM_STRESS_REDUCTION_BUFF_0'",
     "'CM_STEALTH_DAMAGE_BUFF_MIN'",
     "'CM_STEALTH_DAMAGE_BUFF_MAX'",
     "'CM_IGNORE_STEALTH_BUFF'"
